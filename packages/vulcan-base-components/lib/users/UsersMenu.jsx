6da29be781df5fede948d6774101d4f6c3e7fe28
manriquef <cisco.m@outlook.com>
2017-05-26 17:19:57 +0900

    k
    k

--- packages/vulcan-base-components/lib/users/UsersMenu.jsx
+++ packages/vulcan-base-components/lib/users/UsersMenu.jsx
@@ -10,29 +10,7 @@ import { withApollo } from 'react-apollo';
 import FeedsNewForm from 'meteor/nodepeep:post-by-feed';
 
 const UsersMenu = ({currentUser, client}) =>
-  <div className="users-menu">
-    <Dropdown id="user-dropdown">
-      <Dropdown.Toggle>
-        <Components.UsersAvatar size="small" user={currentUser} link={false} />
-        <div className="users-menu-name">{Users.getDisplayName(currentUser)}</div>
-      </Dropdown.Toggle>
-      <Dropdown.Menu>
-        <LinkContainer to={`/users/${currentUser.slug}`}>
-          <MenuItem className="dropdown-item" eventKey="1"><FormattedMessage id="users.profile"/></MenuItem>
-        </LinkContainer>
-        <LinkContainer to={`/account`}>
-          <MenuItem className="dropdown-item" eventKey="2"><FormattedMessage id="users.edit_account"/></MenuItem>
-        </LinkContainer>
-        <MenuItem className="dropdown-item" eventKey="4" onClick={() => Meteor.logout(() => client.resetStore())}><FormattedMessage id="users.log_out"/></MenuItem>
-      </Dropdown.Menu>
-    </Dropdown>
-  </div>
 
-  render() {
-
-    const {currentUser, client} = this.props;
-
-    return (
       <div className="users-menu">
         <Dropdown id="user-dropdown">
           <Dropdown.Toggle>
@@ -53,10 +31,6 @@ const UsersMenu = ({currentUser, client}) =>
           </Dropdown.Menu>
         </Dropdown>
       </div>
-    )
-  }
-
-}
 
 UsersMenu.propsTypes = {
   currentUser: PropTypes.object,
